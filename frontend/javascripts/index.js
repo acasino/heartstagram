f73bcdff74969729affac5a8e56fad3f97ed90db
Andrew <andrew.casin@gmail.com>
2021-06-11 00:49:35 -0500

    Create variables in index.js

---
+++ frontend/javascripts/index.js
@@ -0,0 +1,9 @@
+// List of variables
+let navbar = document.getElementsByClassName('navbar')
+let heartstagram = document.getElementById('heartstagram-logo')
+let profile = document.getElementById('profile')
+let feed = document.getElementById('feed')
+let imagePost = document.getElementById('image-post')
+let login = document.getElementById('login')
+
+let allImages = {}

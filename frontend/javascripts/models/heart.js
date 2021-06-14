a8d06bb009f170161172500817f3a543be32df5e
Andrew <andrew.casin@gmail.com>
2021-06-14 00:56:27 -0500

    Create initial classes

--- frontend/javascripts/models/heart.js
+++ frontend/javascripts/models/heart.js
@@ -1,1 +1,10 @@
 //hearts
+class Heart {
+
+}
+
+// t.integer "heart_count"
+// t.bigint "image_id", null: false
+// t.datetime "created_at", precision: 6, null: false
+// t.datetime "updated_at", precision: 6, null: false
+// t.index ["image_id"], name: "index_hearts_on_image_id"
--- frontend/javascripts/models/image.js
+++ frontend/javascripts/models/image.js
@@ -1,1 +1,15 @@
 //image class
+class Image {
+    constructor(image) {
+        this.caption = image.caption
+
+    }
+    static all = []
+
+}
+
+// t.text "image"
+// t.bigint "user_id", null: false
+// t.datetime "created_at", precision: 6, null: false
+// t.datetime "updated_at", precision: 6, null: false
+// t.string "caption"
